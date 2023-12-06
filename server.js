9098a31eaf0a2cc5ae14c6d9d3b438a8beee237c
Muhammad Kashaf <muhammadkashaf331@gmail.com>
2023-12-06 10:07:26 +0500

    styling applied

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
